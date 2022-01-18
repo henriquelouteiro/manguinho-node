5ff74c8c50813fe6f378045e896a01a4412866d3
Henrique Louteiro <henrique.louteiro@hotmail.com>
2022-01-17 23:39:28 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,7 +16,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
-  test('Should return 400 if no name is provided', () => {
+  test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
